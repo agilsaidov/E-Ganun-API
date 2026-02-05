b0e038b1b75dee1a8261ee41b6bb568548075766
Aqil Səidov <aqilasa638@gmail.com>
2026-02-05 16:19:37 +0400

    Add model and JsonToSqlConverter

---
+++ src/main/java/com/project/e_ganun/model/Ganun.java
@@ -0,0 +1,19 @@
+package com.project.e_ganun.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+    @Data
+    @Entity
+    @Table(name = "ganun")
+    public class Ganun {
+        @Id
+        @Column(name = "ganun_no")
+        private String ganunNo;
+
+        @Column(name = "ganun_text")
+        private String ganunText;
+    }
---
+++ src/main/java/com/project/e_ganun/utils/JsonToSQLConverter.java
@@ -0,0 +1,40 @@
+package com.project.e_ganun.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Map;
+
+public class JsonToSQLConverter {
+    public static void main(String[] args) throws IOException, SQLException {
+        File file = new File("file_path.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(file);
+
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/db_name", "username", "password");
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO ganun(ganun_no, ganun_text) VALUES (?, ?)");
+
+        Iterator<Map.Entry<String, JsonNode>> fields = root.fields();
+
+        while (fields.hasNext()) {
+            Map.Entry<String, JsonNode> entry = fields.next();
+            String key = entry.getKey();
+            String value = entry.getValue().asText();
+
+            ps.setString(1, key);
+            ps.setString(2, value);
+            ps.executeUpdate();
+        }
+        ps.close();
+        conn.close();
+
+        System.out.println("Done");
+    }
+}
