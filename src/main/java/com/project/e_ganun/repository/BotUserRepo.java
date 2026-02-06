4fedd222c9efef14a7290d57888a98c26358aa09
Aqil Səidov <aqilasa638@gmail.com>
2026-02-06 17:35:14 +0400

    Implement repo methods/queries

--- src/main/java/com/project/e_ganun/repository/BotUserRepo.java
+++ src/main/java/com/project/e_ganun/repository/BotUserRepo.java
@@ -1,4 +1,15 @@
 package com.project.e_ganun.repository;
 
-public interface BotUserRepo {
+import com.project.e_ganun.model.BotUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BotUserRepo extends CrudRepository<BotUser, Long> {
+
+    Optional<BotUser> findByTelegramId(Long telegramId);
+
+    boolean existsByTelegramId(Long telegramId);
 }
--- src/main/java/com/project/e_ganun/repository/UsageRepo.java
+++ src/main/java/com/project/e_ganun/repository/UsageRepo.java
@@ -1,4 +1,22 @@
 package com.project.e_ganun.repository;
 
-public interface UsageRepo {
+import com.project.e_ganun.model.Usage;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsageRepo extends CrudRepository<Usage, Long> {
+
+    Optional<Usage> findByTelegramId(Long telegramId);
+
+    @Query(value = "SELECT SUM(total_searches) FROM ganun.usage", nativeQuery = true)
+    Long getTotalSearchesAllUsers();
+
+    @Query(value = "SELECT COUNT(*) FROM ganun.usage " +
+            "WHERE last_search_date > NOW() - INTERVAL '1 day'",
+            nativeQuery = true)
+    Long getActiveUsersToday();
 }
